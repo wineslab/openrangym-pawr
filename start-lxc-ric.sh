2f60c4d1919d92069dc6b567f7fb2ee67445155e
Leonardo Bonati <leonardo.bonati@gmail.com>
2021-10-05 16:08:00 -0400

    Setting postrouting chain for RIC

--- start-lxc-ric.sh
+++ start-lxc-ric.sh
@@ -4,6 +4,7 @@
 set -xeuo pipefail
 
 RIC_LXC_IMG=ric
+RIC_PORT=36422
 
 echo "Initializing LXC container"
 lxc init ${RIC_LXC_IMG} ${RIC_LXC_IMG}
@@ -14,3 +15,12 @@ lxc config set ${RIC_LXC_IMG} security.nesting "yes"
 
 echo "Starting LXC container"
 lxc start ${RIC_LXC_IMG}
+
+echo "Setting postrouting chain"
+HOST_IF=`route -e | grep default | awk -F ' ' '{print $8}'`
+RIC_IF=`lxc exec ${RIC_LXC_IMG} -- bash -c "route -e" | grep default | awk -F ' ' '{print $8}'`
+RIC_IP=`lxc list ${RIC_LXC_IMG} -c 4 --format=csv | grep ${RIC_IF} | awk -F '[()]' '{print $1}'`
+iptables -t nat -A PREROUTING -p sctp -i ${HOST_IF} --dport ${RIC_PORT} -j DNAT --to-destination ${RIC_IP}:${RIC_PORT}
+
+echo "Starting RIC"
+lxc exec ${RIC_LXC_IMG} -- bash -c "cd /root/radio_code/ric_bronze/ric-repository && ./setup-ric.sh "${RIC_IF}

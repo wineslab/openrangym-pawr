48d73a6d5f389f9a8f459003037d826e8af41546
Leonardo Bonati <leonardo.bonati@gmail.com>
2021-10-05 16:23:21 -0400

    Getting RIC interface

--- start-lxc-ric.sh
+++ start-lxc-ric.sh
@@ -18,7 +18,7 @@ lxc start ${RIC_LXC_IMG}
 
 echo "Setting postrouting chain"
 HOST_IF=`route -e | grep default | awk -F ' ' '{print $8}'`
-RIC_IF=`lxc exec ${RIC_LXC_IMG} -- bash -c "route -e" | grep default | awk -F ' ' '{print $8}'`
+RIC_IF=`lxc list ${RIC_LXC_IMG} -c 6 --format=csv | awk -F '[()]' '{print $2}'`
 RIC_IP=`lxc list ${RIC_LXC_IMG} -c 4 --format=csv | grep ${RIC_IF} | awk -F '[()]' '{print $1}'`
 iptables -t nat -A PREROUTING -p sctp -i ${HOST_IF} --dport ${RIC_PORT} -j DNAT --to-destination ${RIC_IP}:${RIC_PORT}
 

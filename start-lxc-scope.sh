3b6739ded153c5118e01298ea0c3530917d8eda6
Leonardo Bonati <leonardo.bonati@gmail.com>
2022-06-10 20:48:14 -0230

    Added Arena configuration

--- start-lxc-scope.sh
+++ start-lxc-scope.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This starts the SCOPE LXC container on POWDER or ORBiT testbeds
 #  Call it as start-lxc-scope.sh testbed usrp_type [flash]
-#    testbed: powder or orbit
+#    testbed: powder, orbit, or arena
 #    usrp_type: b210 or x310
 #    flash: flash USRP X310 if passed
 set -xeuo pipefail
@@ -12,9 +12,11 @@ DU_LXC_BASE_IMG=du-scope
 # testbed images
 DU_LXC_IMG_POWDER=du-scope-1804
 DU_LXC_IMG_ORBIT=${DU_LXC_BASE_IMG}
+DU_LXC_IMG_ARENA=${DU_LXC_BASE_IMG}
 
 # testbed subnetwork for USRP X310
 X310_NET_POWDER=192.168.40.0
+X310_NET_ARENA=192.168.0.0
 
 # check number of passed arguments
 if [[ $# -lt 2 ]]; then
@@ -34,8 +36,11 @@ fi
 if [[ ${TESTBED} == "powder" ]]; then
   DU_LXC_IMG=${DU_LXC_IMG_POWDER}
   X310_NET=${X310_NET_POWDER}
-elif [[ ${TESTBED}=="orbit" ]]; then
+elif [[ ${TESTBED} == "orbit" ]]; then
   DU_LXC_IMG=${DU_LXC_IMG_ORBIT}
+elif [[ ${TESTBED} == "arena" ]]; then
+  DU_LXC_IMG=${DU_LXC_IMG_ARENA}
+  X310_NET=${X310_NET_ARENA}
 else
   echo "Unknown passed testbed."
   exit 1
@@ -57,10 +62,12 @@ if [[ ${USRP} == "b210" ]]; then
   echo "Configuring USB passthrough to LXC container"
   lxc config set ${DU_LXC_IMG} raw.lxc "lxc.cgroup.devices.allow = c 189:* rwm"
   lxc config device add ${DU_LXC_IMG} b210usb usb mode="0777"
-elif [[ ${USRP} == "x310" && ${TESTBED} == "powder" ]]; then
-  echo "Adding Ethernet interface to X310"
-  X310_IF=`route -n | grep ${X310_NET} | awk -F ' ' '{print $8}'`
-  lxc config device add ${DU_LXC_IMG} usrp1 nic name="usrp1" nictype="physical" parent="${X310_IF}"
+elif [[ ${USRP} == "x310" ]]; then
+  if [[ ${TESTBED} == "powder" || ${TESTBED} == "arena" ]]; then
+    echo "Adding Ethernet interface to X310"
+    X310_IF=`route -n | grep ${X310_NET} | awk -F ' ' '{print $8}'`
+    lxc config device add ${DU_LXC_IMG} usrp1 nic name="usrp1" nictype="physical" parent="${X310_IF}"
+  fi
 fi
 
 echo "Configuring container security"
@@ -70,15 +77,16 @@ echo "Starting LXC container"
 lxc start ${DU_LXC_IMG}
 
 if [[ ${USRP} == "x310" ]]; then
-  if [[ ${TESTBED} == "powder" ]]; then
+  if [[ ${TESTBED} == "powder" || ${TESTBED} == "arena" ]]; then
     echo "Configuring Ethernet interface to X310"
     lxc exec ${DU_LXC_IMG} -- bash -c "ifconfig usrp1 192.168.40.1/24 mtu 9000"
     # lxc exec ${DU_LXC_IMG} -- bash -c "ifconfig usrp1 "$(cut -d . -f1-3 <<< $X310_NET)".1/24 mtu 9000"
 
     echo "Downloading UHD images"
+    sleep 1
     lxc exec ${DU_LXC_IMG} -- bash -c "/usr/local/lib/uhd/utils/uhd_images_downloader.py"
 
-    if [[ ${FLASH} == "flash" ]]; then
+    if [[ ${TESTBED} == "powder" && ${FLASH} == "flash" ]]; then
       echo "Flashing USRP device"
       lxc exec ${DU_LXC_IMG} -- bash -c "/usr/local/bin/uhd_image_loader --args=\"type=x300,addr=192.168.40.2,fpga=XG\""
     fi

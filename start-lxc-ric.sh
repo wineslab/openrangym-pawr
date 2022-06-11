f62dd67f306d0af2391943f45be6395124d67719
Leonardo Bonati <leonardo.bonati@gmail.com>
2022-06-10 21:49:26 -0230

    Importing base images

--- start-lxc-ric.sh
+++ start-lxc-ric.sh
@@ -9,13 +9,13 @@ RIC_PORT=36422
 echo "Flushing NAT table"
 iptables -t nat -F
 
-echo "Restarting LXD/LXC"
-if [[ $(service --status-all 2>&1 | grep lxd | wc -l) == "0" ]]; then
-  snap restart lxd
-else
-  service lxd restart
-  service lxc restart
-fi
+#echo "Restarting LXD/LXC"
+#if [[ $(service --status-all 2>&1 | grep lxd | wc -l) == "0" ]]; then
+#  snap restart lxd
+#else
+#  service lxd restart
+#  service lxc restart
+#fi
 
 echo "Initializing LXC container"
 lxc init ${RIC_LXC_IMG} ${RIC_LXC_IMG}
@@ -37,4 +37,5 @@ RIC_IP=$(lxc list ${RIC_LXC_IMG} -c 4 --format=csv | grep ${RIC_IF} | awk -F '[(
 iptables -t nat -A PREROUTING -p sctp -i ${HOST_IF} --dport ${RIC_PORT} -j DNAT --to-destination ${RIC_IP}:${RIC_PORT}
 
 echo "Starting RIC"
+lxc exec ${RIC_LXC_IMG} -- bash -c "cd /root/radio_code/ric_bronze/ric-repository && ./import-base-images.sh"
 lxc exec ${RIC_LXC_IMG} -- bash -c "cd /root/radio_code/ric_bronze/ric-repository && ./setup-ric.sh "${RIC_IF}

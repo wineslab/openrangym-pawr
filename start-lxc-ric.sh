9252cf80df97c2714284e56ce9d9b02b3e36913b
Leonardo Bonati <leonardo.bonati@gmail.com>
2022-06-11 10:40:24 -0600

    RIC script

--- start-lxc-ric.sh
+++ start-lxc-ric.sh
@@ -6,6 +6,9 @@ set -xeuo pipefail
 RIC_LXC_IMG=ric
 RIC_PORT=36422
 
+SCRIPT_PATH="/root/radio_code/colosseum-near-rt-ric/setup-scripts"
+#SCRIPT_PATH="/root/radio_code/ric_bronze/ric-repository"
+
 echo "Flushing NAT table"
 iptables -t nat -F
 
@@ -37,7 +40,7 @@ RIC_IP=$(lxc list ${RIC_LXC_IMG} -c 4 --format=csv | grep ${RIC_IF} | awk -F '[(
 iptables -t nat -A PREROUTING -p sctp -i ${HOST_IF} --dport ${RIC_PORT} -j DNAT --to-destination ${RIC_IP}:${RIC_PORT}
 
 echo "Starting RIC"
-lxc exec ${RIC_LXC_IMG} -- bash -c "cd /root/radio_code/ric_bronze/ric-repository && ./import-base-images.sh"
-lxc exec ${RIC_LXC_IMG} -- bash -c "cd /root/radio_code/ric_bronze/ric-repository && ./setup-ric.sh "${RIC_IF}
+lxc exec ${RIC_LXC_IMG} -- bash -c "cd "${SCRIPT_PATH}" && ./import-base-images.sh"
+lxc exec ${RIC_LXC_IMG} -- bash -c "cd "${SCRIPT_PATH}" && ./setup-ric.sh "${RIC_IF}
 lxc exec ${RIC_LXC_IMG} -- bash -c "docker image prune -f"
 
